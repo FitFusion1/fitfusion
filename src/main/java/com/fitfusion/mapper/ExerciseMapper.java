aefb9b04bd663cd9ad8e0d733f8d26e5f7f43237
권혁진 <gurwls980423@gmail.com>
2025-07-10 14:44:16 +0900

    Revert "html,enum 생성 / html, css 분리완료(Exercise.css) / html에 base.html 적용 완료 /"
    This reverts commit 2290a0ac161fa55c9159d256a4f58e3c18eb8594.

--- src/main/java/com/fitfusion/mapper/ExerciseMapper.java
+++
@@ -1,9 +0,0 @@
-package com.fitfusion.mapper;
-
-import com.fitfusion.vo.Exercise;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ExerciseMapper {
-    void insertExercise(Exercise exercise);
-}
