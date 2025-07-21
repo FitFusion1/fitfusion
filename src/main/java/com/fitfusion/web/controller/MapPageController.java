ccd894d7d9d9bcdd5ff5231d09721d9ae355e0ae
gusehd425 <gusehd425@naver.com>
2025-07-21 16:10:46 +0900

    2025-07-21 헬스장 디테일 페이지 찜 기능 뺴고 구현, 헬스장 비교 별점으로 단순 구현

--- src/main/java/com/fitfusion/web/controller/MapPageController.java
+++ src/main/java/com/fitfusion/web/controller/MapPageController.java
@@ -1,6 +1,7 @@
 package com.fitfusion.web.controller;
 
 import com.fitfusion.dto.DetailDataDto;
+import com.fitfusion.dto.GymReviewDto;
 import com.fitfusion.security.SecurityUser;
 import com.fitfusion.service.KakaoShowGymData;
 import com.fitfusion.vo.User;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,8 +23,13 @@ public class MapPageController {
 
     // 기본 경로 접근
     @GetMapping("/map")
-    public String map() {
-        return "map";
+    public String map(Model model, @AuthenticationPrincipal SecurityUser securityUser) {
+        if (securityUser != null ) {
+            model.addAttribute("loginUserId", securityUser.getUser().getUserId());
+        } else {
+            model.addAttribute("loginUserId", null);
+        }
+        return "healthGym/map";
     }
 
     // 기본 경로 접근
@@ -28,17 +37,33 @@ public class MapPageController {
     public String detail(@PathVariable int gymId, Model model, @AuthenticationPrincipal SecurityUser securityUser) {
 
         DetailDataDto detailData = kakaoShowGymData.detailForm(gymId);
-        User user = securityUser.getUser();
 
-        model.addAttribute("user", user);
-        model.addAttribute("loginUserId", user.getUserId());
+        if (securityUser != null) {
+            User user = securityUser.getUser();
+            model.addAttribute("user", user);
+            System.out.println("userName: " + user.getName());
+            model.addAttribute("loginUserId", user.getUserId());
+            System.out.println("loginUserId: " + user.getUserId());
+        } else {
+            model.addAttribute("loginUserId", null);
+        }
         model.addAttribute("detailData", detailData);
-        return "detail";
+        return "healthGym/detail";
     }
 
 
-    @GetMapping("/compare")
-    public String showComparePage() {
-        return "recentGyms";
+    @GetMapping("/gyms/compare")
+    public String showComparePage(@RequestParam("gymId") List<Integer> gymIds, Model model, @AuthenticationPrincipal SecurityUser securityUser) {
+
+        List<DetailDataDto> gymList = kakaoShowGymData.detailFormList(gymIds);
+
+        if (securityUser != null) {
+            model.addAttribute("loginUserId", securityUser.getUser().getUserId());
+        } else {
+            model.addAttribute("loginUserId", null);
+        }
+        model.addAttribute("detailDataList", gymList);
+
+        return "healthGym/recentGyms";
     }
 }
