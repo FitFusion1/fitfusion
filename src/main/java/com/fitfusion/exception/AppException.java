a6c74f9bbae6bd595fabf72deb28a84b86194f92
soyeon4 <ksyen9@gmail.com>
2025-07-28 14:45:14 +0900

    예외 처리 클래스, 로직 추가. https 오류와 앱 내부 오류 처리

--- src/main/java/com/fitfusion/exception/AppException.java
+++ src/main/java/com/fitfusion/exception/AppException.java
@@ -1,12 +1,13 @@
 package com.fitfusion.exception;
 
-public class AppException extends RuntimeException {
+import org.springframework.http.HttpStatus;
 
+public class AppException extends RuntimeException {
     public AppException(String message) {
         super(message);
     }
-
     public AppException(String message, Throwable cause) {
         super(message, cause);
     }
 }
+
